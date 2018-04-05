0ef66b21a55048b24f66dd2b242b9bc21f7a2f00
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-04-05 19:08:17 -0300

    criando tela de SMS

--- app/src/main/java/com/example/a1401587_0/t1/MensagemActivity.java
+++ app/src/main/java/com/example/a1401587_0/t1/MensagemActivity.java
@@ -3,16 +3,11 @@ package com.example.a1401587_0.t1;
 import android.app.Activity;
 import android.os.Bundle;
 
-import butterknife.ButterKnife;
-
-public class MainActivity extends Activity {
+public class MensagemActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        ButterKnife.bind(this);
+        setContentView(R.layout.activity_mensagem);
     }
-
-
 }
