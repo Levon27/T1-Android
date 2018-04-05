f4c173967aa5d74064968db41b4de61f58f2125c
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-04-05 19:48:38 -0300

    Dank memes funcionando

--- app/src/main/java/com/example/a1401587_0/t1/MainActivity.java
+++ app/src/main/java/com/example/a1401587_0/t1/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.a1401587_0.t1;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
 
@@ -14,5 +19,17 @@ public class MainActivity extends Activity {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.btnMemes)
+    public void Memes(){
+        Uri uri = Uri.parse("https://www.google.com.br/search?q=jojo's_dank_memes&source=lnms&tbm=isch");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
 
+    private void debug(String s){
+        Toast.makeText(getApplicationContext(),
+                s,
+                Toast.LENGTH_SHORT)
+                .show();
+    }
 }
