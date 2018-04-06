fcfdca93b4d1a096e078f4ff6358a2e4fc8c1caa
Aluno <Aluno>
2018-04-06 17:50:54 -0300

    Desenvolvendo busca de contato

--- app/src/main/java/com/example/a1401587_0/t1/MensagemActivity.java
+++ app/src/main/java/com/example/a1401587_0/t1/MensagemActivity.java
@@ -1,8 +1,11 @@
 package com.example.a1401587_0.t1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -10,6 +13,7 @@ import butterknife.OnClick;
 
 public class MensagemActivity extends Activity {
     @BindView(R.id.btnContato) Button btnContato;
+    @BindView(R.id.etContato) EditText etContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,11 @@ public class MensagemActivity extends Activity {
 
     @OnClick(R.id.btnContato)
     public void procuraContato(){
-
+        String contato = etContato.getText().toString();
+        Intent intent = new Intent (Intent.ACTION_PICK);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_PHONE_NUMBER);
+        }
     }
 }
